5bd62266b0286d9bad5b205cd32426fa73ea8632
shawn <shawnantonucci2018@gmail.com>
2019-03-21 11:49:05 -0700

    created userbar and tempNav

--- src/InstaClone.js
+++ src/InstaClone.js
@@ -5,7 +5,25 @@ class InstaClone extends Component {
   render() {
     return (
       <View style={{ flex: 1, width: 100 + "%", height: 100 + "%" }}>
-        <View style={styles.tempNav} />
+        <View style={styles.tempNav}>
+          <Text>InstaClone</Text>
+        </View>
+
+        <View style={styles.userBar}>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Image
+              style={styles.userPic}
+              source={{
+                uri:
+                  "https://lh3.googleusercontent.com/8EV9X_4gFvmiIF5hv20RUfJurdGB-sGBMKaLBIjmyEZmcp4f8RTnCGFr3z3b53cZeYEzsiBE4Y9vOwIEQIzX4rrQsg"
+              }}
+            />
+            <Text style={{ marginLeft: 10 }}>RyanABryne</Text>
+          </View>
+          <View style={{ alignItems: "center" }}>
+            <Text style={{ fontSize: 30 }}>...</Text>
+          </View>
+        </View>
 
         <Image
           style={{ width: 100 + "%", height: 100 }}
@@ -23,7 +41,26 @@ const styles = StyleSheet.create({
   tempNav: {
     width: 100 + "%",
     height: 75,
-    backgroundColor: "rgb(250,250,250)"
+    backgroundColor: "rgb(250,250,250)",
+    borderBottomColor: "rgb(233,233,233)",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  userBar: {
+    width: 100 + "%",
+    height: 50,
+    backgroundColor: "white",
+    flexDirection: "row",
+    paddingHorizontal: 10,
+    justifyContent: "space-between"
+  },
+
+  userPic: {
+    height: 40,
+    width: 40,
+    borderRadius: 20
   }
 });
 
