c85718dd2cecafed25adffbcdb54fb1578efb09b
shawn <shawnantonucci2018@gmail.com>
2019-03-21 14:05:22 -0700

    added user post image and styling

--- src/InstaClone.js
+++ src/InstaClone.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, Dimensions } from "react-native";
 
 class InstaClone extends Component {
+  constructor() {
+    super();
+    this.state = {
+      screenWidth: Dimensions.get("window").width
+    };
+  }
+
   render() {
+    const imageHeight = Math.floor(this.state.screenWidth * 1.1);
+    const imageUri =
+      "https://lh3.googleusercontent.com/yQ82fiiVQVa9sMZa2mTHbMH6W0TRlwJ8nMJnDVVV_Xd9ebr_zyUcmEgwmE_pUVUWPYSujVJ2XC8UDtF6cZpb2HrgKw" + "=s" + imageHeight + "-c";
+
     return (
       <View style={{ flex: 1, width: 100 + "%", height: 100 + "%" }}>
         <View style={styles.tempNav}>
@@ -26,10 +37,9 @@ class InstaClone extends Component {
         </View>
 
         <Image
-          style={{ width: 100 + "%", height: 100 }}
+          style={{ width: this.state.screenWidth, height: 425 }}
           source={{
-            uri:
-              "https://lh3.googleusercontent.com/yQ82fiiVQVa9sMZa2mTHbMH6W0TRlwJ8nMJnDVVV_Xd9ebr_zyUcmEgwmE_pUVUWPYSujVJ2XC8UDtF6cZpb2HrgKw"
+            uri: imageUri
           }}
         />
       </View>
@@ -40,7 +50,7 @@ class InstaClone extends Component {
 const styles = StyleSheet.create({
   tempNav: {
     width: 100 + "%",
-    height: 75,
+    height: 55,
     backgroundColor: "rgb(250,250,250)",
     borderBottomColor: "rgb(233,233,233)",
     borderBottomWidth: StyleSheet.hairlineWidth,
